d8e9589177cb3bfb12acd7134a88af01f42a6fba
Michael Younkin <younkin.michael@gmail.com>
2016-07-15 18:34:54 -0400

    Double quotes seem better than single quotes

--- lambda/index.js
+++ lambda/index.js
@@ -1,26 +1,26 @@
-'use strict';
-let child_process = require('child_process');
+"use strict";
+let child_process = require("child_process");
 
-process.env['PATH'] =
-    process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'];
+process.env["PATH"] =
+    process.env["PATH"] + ":" + process.env["LAMBDA_TASK_ROOT"];
 
 exports.handler = (event, context, callback) => {
     if (!event.regex) {
-        callback('Please specify a regex.');
+        callback("Please specify a regex.");
         return;
     }
     if (!event.sampleInput) {
-        callback('Please specify sample input.');
+        callback("Please specify sample input.");
         return;
     }
-    let args = event.sampleInput.split('\n')
+    let args = event.sampleInput.split("\n")
     args.splice(0, 0, event.regex)
-    const child = child_process.execFile('regexer', args,
+    const child = child_process.execFile("regexer", args,
                                          (error, stdout, stderr) => {
         if (error) {
             callback(error, stderr);
         } else {
-            callback(null, stdout.split('\n'));
+            callback(null, stdout.split("\n"));
         }
     });
 };
